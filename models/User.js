d8c818743f8373ccdf2b2bc1a2a265dda417551b
Lightfooted <vesselofbalance@hotmail.com>
2021-10-18 12:23:16 -0400

    user model mostly done. need email validation

--- models/User.js
+++ models/User.js
@@ -1,9 +1,40 @@
 const { Schema, model } = require('mongoose');
-//TO DO: Pick dateformat
+//TO DO: Pick dateformat and add email "match" validation
 
-const UserSchema = new Schema()
-//TO DO: username, email, thoughts and friends
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+			unique: true,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+			unique: true
+    },
+    thoughts: {
+      type: Schema.Types.ObjectId,
+      ref: 'Thought'
+    },
+    friends: {
+      type: Schema.Types.ObjectId,
+			ref: 'User'
+    }
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
 
+UserSchema.virtual('friendCount').get(function() {
+  return this.friends.length;
+});
 
 const User = model('User', UserSchema);
 
